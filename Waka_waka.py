f9f9b41b7b89f3126cc6c3b41a1dc0cac12dabbd
Runeofsnow <61072637+Runeofsnow@users.noreply.github.com>
2020-02-15 00:18:03 -0500

    Background and yellow dot added
    Just starting to make things

---
+++ Waka_waka.py
@@ -0,0 +1,31 @@
+#Authors: Rune
+
+#Summon turtle
+import turtle
+import math
+import random
+import time
+
+#Make background
+wn = turtle.Screen()
+wn.bgcolor("dark blue")
+
+# Game Title
+wn.title("Waka waka")
+
+#PizzaMan
+skk = turtle.Turtle()
+# taking input for the radius of the circle
+pizza = turtle.Turtle()
+pizza.shape('circle')
+pizza.color('yellow')
+pizza.speed(0)
+pizza.penup()
+
+
+
+
+
+
+
+turtle.mainloop()
