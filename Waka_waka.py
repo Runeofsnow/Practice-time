70e1bc967531a8695881b7a61bbaf40c64e50d0f
Runeofsnow <61072637+Runeofsnow@users.noreply.github.com>
2020-02-19 23:19:47 -0500

    Added a gif as the turtle and movement
    The gif isn't actually moving though the turtle does more with directional imput

--- Waka_waka.py
+++ Waka_waka.py
@@ -2,9 +2,14 @@
 
 #Summon turtle
 import turtle
-import math
 import random
-import time
+
+turtle.hideturtle()
+
+
+#Named constant
+SCREEN_WIDTH= 800
+SCREEN_HEIGHT = 800
 
 #Make background
 wn = turtle.Screen()
@@ -13,19 +18,49 @@ wn.bgcolor("dark blue")
 # Game Title
 wn.title("Waka waka")
 
+#Make a maze
+
+
 #PizzaMan
-skk = turtle.Turtle()
+
+#Pacman gif as turtle go!
+screen = turtle.Screen()
+screen.register_shape('Pizzaman.gif')
+
 # taking input for the radius of the circle
 pizza = turtle.Turtle()
-pizza.shape('circle')
+pizza.shape('Pizzaman.gif')
 pizza.color('yellow')
 pizza.speed(0)
 pizza.penup()
+pizza.speed(0)
+pizza.width(5)
+
+#Define directions
+def up ():
+    pizza.setheading(90)
+    pizza.forward(5)
 
 
+def down ():
+    pizza.setheading(270)
+    pizza.forward(5)
 
+def left ():
+    pizza.setheading(180)
+    pizza.forward(5)
 
+def right ():
+    pizza.setheading(0)
+    pizza.forward(5)
 
+#Input and receive instructions
+turtle.listen()
+
+turtle.onkey(up, 'Up')
+turtle.onkey(down, 'Down')
+turtle.onkey(left, 'Left')
+turtle.onkey(right, 'Right')
 
 
 turtle.mainloop()
